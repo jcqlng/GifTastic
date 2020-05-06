e8d2bf586eddb658baa3707156025de14c4be737
jcqlng <jcqlngeiger@outlook.com>
2020-05-06 12:24:05 -0400

    started variables for giphy search. created a still response.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,9 +18,28 @@ function populateButtons(searchArray,classToAdd,areaToAddTo){
         a.text(searchArray[i]),
         $(areaToAddTo).append(a);
     }
-}
+};
 
 // Click function for search and adding API 
 $(document).on('click', '.searchButton', function(){
+    var type = $(this).data('type'),
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=OedtgRRnjivPVer7xxzeP6uSTRH3iYrw&q=&limit=25&offset=0&rating=G&lang=en'
+    // AJAX to retrieve the gif images from the website
+    $.ajax({url:queryURL, method:'GET'})
+    .done(function(response){
+        //For loop 
+        for(var i=0; i>response.data.length;i++){
+            var searchDiv = $('div class="search-item">');
+            var rating = response.data[i].rating;
+            var p= $('<p>').text('rating:' +rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image =$('<img>');
+            image.attr('src',still);
+            
+
+        }
+
+    })
 
 })
