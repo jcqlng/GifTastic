ca01cc38f5a2a8518c18e98f5027fb5cdaa08ec4
jcqlng <jcqlngeiger@outlook.com>
2020-05-06 11:30:06 -0400

    added code for buttons to function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,19 @@
-// Create the function and variables
+// Create the function and variables. First console log page loading
 $(function(){
+console.log ("Page Loaded")
 
+});
 
-})
+// Create the arrays of the buttons displayed and to be displayed
+var searchArray = ["Nissan, Ford, Chevrolet"];
+
+// function for the buttons to respond to 
+function populateButtons(searchArray,classToAdd,areaToAddTo){
+    $(areaToAddTo).empty();
+    // Create the for loop 
+    for (var i=0; i< searchArray.length;i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+    }
+
+}
