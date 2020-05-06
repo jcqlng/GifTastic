010db3cb516b72c1aa023b03c280b7dcfc1b236d
jcqlng <jcqlngeiger@outlook.com>
2020-05-06 11:36:06 -0400

    created append function to add other searches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,10 @@ function populateButtons(searchArray,classToAdd,areaToAddTo){
     for (var i=0; i< searchArray.length;i++){
         var a = $('<button>');
         a.addClass(classToAdd);
+        a.attr('data-type',searchArray[i]),
+        a.text(searchArray[i]),
+        $(areaToAddTo).append(a);
+
     }
 
 }
